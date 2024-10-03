aa9dba539e0c88fae7eb67e3b6da3c9ceda4a0eb
adam-aswin <aswinsachu2118@gmal.com>
2024-10-03 15:56:50 +0530

    updated

--- newproject/lib/components/Cartpage.dart
+++ newproject/lib/components/Cartpage.dart
@@ -11,11 +11,13 @@ class Cartpage extends StatefulWidget {
 class _CartpageState extends State<Cartpage> {
   final _coffees = Hive.box('coffees');
   List coffee = [];
+  num sum = 0;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     getData();
+    total();
   }
 
   void getData() {
@@ -23,6 +25,14 @@ class _CartpageState extends State<Cartpage> {
       coffee = _coffees.get('key');
     } else {
       null;
+    }
+  }
+
+  void total() {
+    for (Map i in coffee) {
+      print(i["price"]);
+      sum = sum + i["price"] * i["qty"];
+      print(sum);
     }
   }
 
@@ -129,24 +139,61 @@ class _CartpageState extends State<Cartpage> {
                           height: 8,
                         ),
                         Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Image.asset(
-                              "./lib/icons/add-button.png",
-                              height: 23,
-                              width: 23,
+                            Text(
+                              "₹ ${(coffee[index]["price"] * coffee[index]["qty"]).toString()}",
+                              style: TextStyle(
+                                fontFamily: "Kanit",
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15.2,
+                              ),
                             ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(coffee[index]["qty"].toString()),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Image.asset(
-                              "./lib/icons/add-button.png",
-                              height: 23,
-                              width: 23,
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                coffee[index]["qty"] <= 0
+                                    ? Image.asset(
+                                        "lib/icons/subtraction (1).png",
+                                        height: 21,
+                                        width: 21,
+                                      )
+                                    : GestureDetector(
+                                        onTap: () {
+                                          setState(() {
+                                            coffee[index]["qty"]--;
+                                            sum = sum - coffee[index]["price"];
+                                            // minus();
+                                          });
+                                        },
+                                        child: Image.asset(
+                                          "./lib/icons/subtraction.png",
+                                          height: 21,
+                                          width: 21,
+                                        ),
+                                      ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(coffee[index]["qty"].toString()),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      coffee[index]["qty"]++;
+                                      sum = sum + coffee[index]["price"];
+                                      // total();
+                                    });
+                                  },
+                                  child: Image.asset(
+                                    "./lib/icons/add-button.png",
+                                    height: 23,
+                                    width: 23,
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
                         )
@@ -156,6 +203,115 @@ class _CartpageState extends State<Cartpage> {
                 },
               ),
             ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 150,
+              margin: EdgeInsets.only(right: 15, left: 15),
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 5,
+                      spreadRadius: 0,
+                      offset: Offset(5, 5),
+                      color: const Color.fromARGB(255, 116, 81, 69),
+                    ),
+                  ]),
+              child: Column(
+                children: [
+                  Expanded(
+                      child: ListView.builder(
+                    padding: EdgeInsets.zero,
+                    itemCount: 1,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Item Total",
+                                style: TextStyle(
+                                  fontFamily: "Kanit",
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15.2,
+                                ),
+                              ),
+                              Text(
+                                "₹ ${sum.toString()}",
+                                style: TextStyle(
+                                  fontFamily: "Kanit",
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15.2,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Taxes",
+                                style: TextStyle(
+                                  fontFamily: "Kanit",
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15.2,
+                                ),
+                              ),
+                              Text(
+                                "₹ ${(sum * 5 / 100).toString()}",
+                                style: TextStyle(
+                                  fontFamily: "Kanit",
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15.2,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                              "________________________________________________"),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "To Pay",
+                                style: TextStyle(
+                                  fontFamily: "Kanit",
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15.2,
+                                ),
+                              ),
+                              Text(
+                                "₹ ${((sum) + (sum * 5 / 100)).toString()}",
+                                style: TextStyle(
+                                  fontFamily: "Kanit",
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15.2,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      );
+                    },
+                  ))
+                ],
+              ),
+            )
           ],
         ),
       ),
