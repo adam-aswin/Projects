e267fb662a1b95a6da309c1ff3d5ef8a8a3754f4
adam-aswin <aswinsachu2118@gmal.com>
2024-10-10 15:43:15 +0530

    updated

--- ecommerce/lib/Contentpage.dart
+++ ecommerce/lib/Contentpage.dart
@@ -9,8 +9,9 @@ class Contentpage extends StatefulWidget {
 }
 
 class _ContentpageState extends State<Contentpage> {
-  int index = 0;
+  int? ind;
   List data = [];
+  List img = [];
   final mydata = Hive.box('products');
   @override
   void initState() {
@@ -25,7 +26,7 @@ class _ContentpageState extends State<Contentpage> {
 
   @override
   Widget build(BuildContext context) {
-    index = int.parse(ModalRoute.of(context)?.settings.arguments as String);
+    ind = int.parse(ModalRoute.of(context)?.settings.arguments as String);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -72,205 +73,221 @@ class _ContentpageState extends State<Contentpage> {
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: ListView(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * .6,
-              color: Colors.grey[100],
-              child: Image.network(
-                data[index]["images"][0],
-                fit: BoxFit.contain,
-              ),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  data[index]["brand"] != null
-                      ? Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              data[index]["brand"],
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Icon(
-                                  Icons.star,
-                                  size: 20,
-                                  color: data[index]["rating"] >= 4
-                                      ? Colors.green
-                                      : data[index]["rating"] >= 3
-                                          ? Colors.orange
-                                          : Colors.red,
-                                ),
-                                Text(data[index]["rating"].toString())
-                              ],
-                            )
-                          ],
-                        )
-                      : Text(
-                          "",
-                          style: TextStyle(
-                            fontSize: 0,
-                          ),
+        child: Expanded(
+          child: ListView(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * .6,
+                color: Colors.grey[100],
+                child: Expanded(
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: data[ind!]["images"].length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height * .6,
+                        color: Colors.grey[100],
+                        child: Image.network(
+                          data[ind!]["images"][index],
+                          fit: BoxFit.contain,
                         ),
-                  data[index]["brand"] != null
-                      ? Text(
-                          data[index]["description"],
-                          style: TextStyle(
-                            fontSize: 13,
-                            color: Colors.black87,
-                          ),
-                        )
-                      : Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              data[index]["title"],
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Icon(
-                                  Icons.star,
-                                  size: 20,
-                                  color: data[index]["rating"] >= 4
-                                      ? Colors.green
-                                      : data[index]["rating"] >= 3
-                                          ? Colors.orange
-                                          : Colors.red,
-                                ),
-                                Text(
-                                  data[index]["rating"].toString(),
-                                )
-                              ],
-                            ),
-                          ],
-                        ),
-                  data[index]["brand"] != null
-                      ? Text(
-                          "",
-                          style: TextStyle(
-                            fontSize: 0,
-                          ),
-                        )
-                      : Text(
-                          data[index]["description"],
-                          style: TextStyle(
-                            fontSize: 13,
-                            color: Colors.black87,
-                          ),
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                  SizedBox(
-                    height: 10,
+                        // child: Text(data[ind!]["reviews"][index]["comment"]),
+                      );
+                    },
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    data[ind!]["brand"] != null
+                        ? Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
                               Text(
-                                "\$ ${data[index]["price"].toString()}",
+                                data[ind!]["brand"],
                                 style: TextStyle(
-                                  decoration: TextDecoration.lineThrough,
-                                  color: const Color.fromARGB(220, 0, 0, 0),
-                                  fontSize: 13.5,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
                               Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
                                 children: [
+                                  Icon(
+                                    Icons.star,
+                                    size: 20,
+                                    color: data[ind!]["rating"] >= 4
+                                        ? Colors.green
+                                        : data[ind!]["rating"] >= 3
+                                            ? Colors.orange
+                                            : Colors.red,
+                                  ),
+                                  Text(data[ind!]["rating"].toString())
+                                ],
+                              )
+                            ],
+                          )
+                        : Text(
+                            "",
+                            style: TextStyle(
+                              fontSize: 0,
+                            ),
+                          ),
+                    data[ind!]["brand"] != null
+                        ? Text(
+                            data[ind!]["description"],
+                            style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.black87,
+                            ),
+                          )
+                        : Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                data[ind!]["title"],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Icon(
+                                    Icons.star,
+                                    size: 20,
+                                    color: data[ind!]["rating"] >= 4
+                                        ? Colors.green
+                                        : data[ind!]["rating"] >= 3
+                                            ? Colors.orange
+                                            : Colors.red,
+                                  ),
                                   Text(
-                                    "\$",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                    ),
+                                    data[ind!]["rating"].toString(),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                    data[ind!]["brand"] != null
+                        ? Text(
+                            "",
+                            style: TextStyle(
+                              fontSize: 0,
+                            ),
+                          )
+                        : Text(
+                            data[ind!]["description"],
+                            style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.black87,
+                            ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "\$ ${data[ind!]["price"].toString()}",
+                                  style: TextStyle(
+                                    decoration: TextDecoration.lineThrough,
+                                    color: const Color.fromARGB(220, 0, 0, 0),
+                                    fontSize: 13.5,
                                   ),
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                  Container(
-                                    width: 40,
-                                    height: 20,
-                                    child: Text(
-                                      "${(data[index]["price"] - (data[index]["price"] * (data[index]["discountPercentage"] / 100))).toString()}",
-                                      overflow: TextOverflow.clip,
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      "\$",
                                       style: TextStyle(
                                         fontWeight: FontWeight.bold,
                                       ),
                                     ),
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Text(
-                            "↓${data[index]["discountPercentage"].toString()} %",
-                            style: TextStyle(
-                              color: Colors.green,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    Container(
+                                      width: 40,
+                                      height: 20,
+                                      child: Text(
+                                        "${(data[ind!]["price"] - (data[ind!]["price"] * (data[ind!]["discountPercentage"] / 100))).toString()}",
+                                        overflow: TextOverflow.clip,
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
                             ),
-                          )
-                        ],
-                      ),
-                      data[index]["stock"] > 10
-                          ? Text(
-                              "In Stock",
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              "↓${data[ind!]["discountPercentage"].toString()} %",
                               style: TextStyle(
                                 color: Colors.green,
                                 fontWeight: FontWeight.bold,
+                                fontSize: 16,
                               ),
                             )
-                          : Row(
-                              children: [
-                                Text(
-                                  "Stock left: ",
-                                  style: TextStyle(
-                                    color: data[index]["stock"] > 10
-                                        ? Colors.green
-                                        : Colors.red,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                          ],
+                        ),
+                        data[ind!]["stock"] > 10
+                            ? Text(
+                                "In Stock",
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  fontWeight: FontWeight.bold,
                                 ),
-                                Text(
-                                  data[index]["stock"].toString(),
-                                  style: TextStyle(
-                                    color: data[index]["stock"] > 10
-                                        ? Colors.green
-                                        : Colors.red,
-                                    fontWeight: FontWeight.bold,
+                              )
+                            : Row(
+                                children: [
+                                  Text(
+                                    "Stock left: ",
+                                    style: TextStyle(
+                                      color: data[ind!]["stock"] > 10
+                                          ? Colors.green
+                                          : Colors.red,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
-                                ),
-                              ],
-                            )
-                    ],
-                  ),
-                ],
-              ),
-            )
-          ],
+                                  Text(
+                                    data[ind!]["stock"].toString(),
+                                    style: TextStyle(
+                                      color: data[ind!]["stock"] > 10
+                                          ? Colors.green
+                                          : Colors.red,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              )
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
