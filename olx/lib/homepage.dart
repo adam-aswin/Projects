e35f991083426b93c2abb384c6a7168ad810e4df
adam-aswin <aswinsachu2118@gmail.com>
2024-10-24 18:00:36 +0530

    updated

--- olx/lib/homepage.dart
+++ olx/lib/homepage.dart
@@ -13,7 +13,9 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   final mydata = Hive.box('mydata');
-  Map data={};
+  Map data = {};
+  List products = [];
+  String? img;
   @override
   void initState() {
     // TODO: implement initState
@@ -28,22 +30,74 @@ class _HomepageState extends State<Homepage> {
       headers: {"Authorization": 'Bearer ${token}'},
     );
     // print(response.body);
-    data = jsonDecode(response.body);
-    print(data);
+    setState(() {
+      data = jsonDecode(response.body);
+      products = data["products1"];
+    });
+    print(products);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Center(
-          child: Text(
-            data["products1"][0]["pname"],
-          ),
-        ),
+      backgroundColor: const Color.fromARGB(242, 255, 255, 255),
+      appBar: AppBar(
+        backgroundColor: Colors.grey[400],
       ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[400],
+      ),
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: GridView.builder(
+            padding: EdgeInsets.only(top: 30, left: 15, right: 15, bottom: 15),
+            itemCount: products.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 2.3 / 3,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10),
+            itemBuilder: (context, index) {
+              return Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 5,
+                      spreadRadius: 1,
+                      offset: Offset(5, 5),
+                      color: Colors.grey,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 130,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey[300],
+                      ),
+                      child: Image.asset(
+                        ,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    Text(
+                      products[index]["pname"],
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          )),
     );
   }
 }
